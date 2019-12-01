6ac7dce154b911de23460dbdaae478ff611178d1
sink66 <sink66@users.noreply.github.com>
2019-12-02 08:36:40 +0900

    reproduced

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,10 +1,10 @@
 require "image_processing/mini_magick"
 
 class ImageUploader < Shrine
-  plugin :derivatives
-  plugin :validation
+  plugin :default_storage, cache: :test_cache, store: :test_store
   plugin :validation_helpers
   plugin :remove_invalid
+  plugin :derivatives
 
   Attacher.derivatives do |original|
     magick = ImageProcessing::MiniMagick.source(original)
@@ -16,6 +16,10 @@ class ImageUploader < Shrine
 
   Attacher.validate do
     # DEBUG: always fail
-    #errors << "fail"
+    errors << "fail"
+    # filepath = file.download.path
+    # if true
+    #   errors << "スキャンをパスできませんでした。スキャン自体に失敗したか、画像ファイルが安全でないと判定されました。"
+    # end
   end
 end
--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -2,8 +2,8 @@ require "shrine"
 require "shrine/storage/file_system"
 
 Shrine.storages = { 
-  cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary 
-  store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent 
+  test_cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"), # temporary 
+  test_store: Shrine::Storage::FileSystem.new("public", prefix: "uploads"),       # permanent 
 }
 
 class Shrine::Attacher
